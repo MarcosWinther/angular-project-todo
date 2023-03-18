476d2af760c5b113a29f67644417e2b600cbafd9
MarcosWinther <winthermarcos@gmail.com>
2023-03-17 23:36:45 -0300

    Criação das funcionalidades de cadastrar tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,6 +11,10 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent {
   public taskList: Array<TaskList> = [];
 
+  public setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false });
+  }
+
   public deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
   }
